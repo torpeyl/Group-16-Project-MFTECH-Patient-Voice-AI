02fca7ef7ac1e289e78e6fb0514b1d9c1a81f569
torpeyl <lt321@ic.ac.uk>
2024-05-01 12:40:19 +0100

    Adding audio playing file

---
+++ playAudio.py
@@ -0,0 +1,12 @@
+import sounddevice as sd
+import re
+
+filename = 'data/voice001.txt'
+
+with open(filename) as f:
+    li = f.readlines()
+    li = [re.sub(r'[\n]', '', s) for s in li]
+    li = [float(i) for i in li]
+
+sd.play(li, 8000)
+sd.wait()
