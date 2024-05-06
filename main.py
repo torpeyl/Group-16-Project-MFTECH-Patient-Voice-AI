19e22617df8c292ac94807cd1175bf971e875d82
torpeyl <lt321@ic.ac.uk>
2024-05-06 10:02:23 +0100

    Adding a counter for how often a value occurs

--- main.py
+++ main.py
@@ -1,10 +1,12 @@
 """
 This program processes all of the info.txt files and prints all of the different values in the data for each data field.
+The number to the right of each data value is the number of times it occurs in the dataset.
 In order to use change the directory to the location of your downloaded data.
 """
 
 import os
 import re
+from colorama import Fore, Style
 
 directory = 'data'
 
@@ -28,11 +30,19 @@ for file in file_names:
             li = pair.split(':')
             # check if key exists, if not create set for that and add first value
             if li[0] in info_dict.keys():
-                info_dict[li[0]].add(li[1])
+                if li[1] in info_dict[li[0]].keys():
+                    info_dict[li[0]][li[1]] += 1
+                else:
+                    info_dict[li[0]][li[1]] = 1
             else:
-                info_dict[li[0]] = {li[1]}
+                info_dict[li[0]] = {li[1]: 1}
 
 
 keys = ['ID', 'Age']        # NOTE THAT THE DATA FIELDS IN THIS LIST WILL NOT BE PRINTED TO 
                             # THE TERMINAL, HOWEVER THEY ARE STILL PRESENT IN THE DATA 
-print({x: info_dict[x] for x in info_dict if x not in keys})
+for key in info_dict.keys():
+    if key not in keys:
+        print(Fore.BLUE + key + Style.RESET_ALL)
+        print(info_dict[key])
+
+# print({x: info_dict[x] for x in info_dict if x not in keys})
